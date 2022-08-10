7938e29c10b6bca9526b538dbfc78cabac866788
DaikiSakuragi <daikisakuragi@outlook.com>
2022-08-10 18:07:59 -0300

    First Commit

---
+++ ExemploAspNetMvc/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using ExemploAspNetMvc.Models;
+
+namespace ExemploAspNetMvc.Controllers;
+
+public class HomeController : Controller /* Herança (HomeController extende a Classe Controller*/
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index() 
+    {
+        return View(); /*Chamando a função view*/
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    public IActionResult PrimeiraAction()
+    {
+        return View();
+    }
+
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
